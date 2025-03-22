2e64d949c01804b75e9b1d33cb69df222c7ddc8c
Baratov Viktor <vitya.baratov@gmail.com>
2025-03-22 21:10:33 +0300

    Login fix

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,17 +1,31 @@
 import { FC, SyntheticEvent, useEffect, useState } from 'react';
 import { LoginUI } from '@ui-pages';
-import { getErrorSelector, loginUser } from '../../services/slices/userSlice';
+import {
+  getErrorSelector,
+  getUserSelector,
+  loginUser
+} from '../../services/slices/userSlice';
 import { useSelector, useDispatch } from '../../services/store';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export const Login: FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const error = useSelector(getErrorSelector);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector(getUserSelector);
+
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
     dispatch(loginUser({ email: email, password: password }));
   };
+
+  useEffect(() => {
+    if (user.name) {
+      navigate('/');
+    }
+  }, [user, navigate]);
 
   return (
     <LoginUI
