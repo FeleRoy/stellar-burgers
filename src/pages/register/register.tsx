947777c7ad5164a645ff7ce527abf42d651a7130
Baratov Viktor <vitya.baratov@gmail.com>
2025-03-23 11:42:19 +0300

    protected route some fix

--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,18 +1,38 @@
-import { FC, SyntheticEvent, useState } from 'react';
+import { FC, SyntheticEvent, useEffect, useState } from 'react';
 import { RegisterUI } from '@ui-pages';
+import { useDispatch, useSelector } from '../../services/store';
+import { useNavigate } from 'react-router-dom';
+import {
+  getErrorSelector,
+  getUserSelector,
+  registerUser
+} from '../../services/slices/userSlice';
 
 export const Register: FC = () => {
   const [userName, setUserName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const error = useSelector(getErrorSelector);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector(getUserSelector);
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
+    dispatch(
+      registerUser({ name: userName, email: email, password: password })
+    );
   };
+
+  useEffect(() => {
+    if (user.name) {
+      navigate('/');
+    }
+  }, [user, navigate]);
 
   return (
     <RegisterUI
-      errorText=''
+      errorText={error}
       email={email}
       userName={userName}
       password={password}
